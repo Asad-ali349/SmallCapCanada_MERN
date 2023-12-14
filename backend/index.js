56bcdbe80c7b41819fce78e564fa90e4d08e671b
Asad-ali349 <asadking066@gmail.com>
2023-12-14 18:31:49 +0500

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -62,8 +62,8 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 //   next();
 // });
 
-app.use(cors({ origin: 'https://smallcapcanada.vercel.app',methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], }));
-// app.use(cors());
+// app.use(cors({ origin: 'https://smallcapcanada.vercel.app/',methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], }));
+app.use(cors());
 
 //Defining Port on which our page is load.
 const PORT = process.env.PORT || 5000;
